7e64402dcf6da9d384c23d31722bacdd96ffdd38
Tuyen Nguyen <tuyennn@ihands.com.vn>
2023-09-22 22:04:37 +0700

    Update function binary_op_any

--- extension/datadocs/include/datadocs.hpp
+++ extension/datadocs/include/datadocs.hpp
@@ -4,6 +4,7 @@
 #include "vector_proxy.hpp"
 
 struct yyjson_val;
+struct yyjson_alc;
 
 namespace duckdb {
 
@@ -16,8 +17,12 @@ extern const LogicalType DDNumericType;
 extern const LogicalType DDVariantType;
 
 extern bool IsDatetime(LogicalType type);
+extern bool IsDecayableType(LogicalType type);
+extern Value GetDecayVariantValue(Value v, yyjson_alc *alc);
+extern Value GetDecayJsonValue(Value v, yyjson_alc *alc);
 extern bool VariantReadScalar(VectorWriter &result, yyjson_val *val, LogicalType type, bool is_list, yyjson_val *info);
 extern bool VariantToJson(VectorWriter &result, Value v);
+bool VariantWriteValue(VectorWriter &result, Value v);
 extern LogicalType ConvertLogicalTypeFromString(std::string type);
 extern LogicalType ConvertLogicalTypeFromJson(yyjson_val *type_info);
 
