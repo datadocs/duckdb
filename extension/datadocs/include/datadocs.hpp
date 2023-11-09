fd29bf0bd0946feff194b6f9c5ed9032a129f806
Tuyen Nguyen <tuyennn@ihands.com.vn>
2023-11-09 13:33:12 +0700

    Update with ci and keys_ci

--- extension/datadocs/include/datadocs.hpp
+++ extension/datadocs/include/datadocs.hpp
@@ -11,6 +11,53 @@ namespace duckdb {
 constexpr int dd_numeric_width = 38;
 constexpr int dd_numeric_scale = 9;
 
+/**
+ * @brief name of optional arguments for function `comparison_any`, `any_in_array` and `sort_hash`
+ *
+ */
+enum class ComparisonArgumentType : uint8_t { ci, keys_ci };
+
+static string ci_str = "ci";
+static string keys_ci_str = "keys_ci";
+
+/**
+ * @brief Default value of optional arguments
+ * ci: true
+ * keys_ci: true
+ */
+static bool ci_default_value = true;
+static bool keys_ci_default_value = true;
+
+/**
+ * @brief Comparison type, ranking types for compare between types in case values couldn't convertible
+ *
+ */
+enum class ComparisonType : uint8_t {
+	INVALID,
+	C_NULL,
+	BOOL,
+	NUMERIC,
+	STRING,
+	BYTES,
+	TIME,
+	DATETIME,
+	INTERVAL,
+	GEO,
+	LIST,
+	STRUCT
+};
+
+/**
+ * @brief Result constant for `comparison_any` function
+ * 0: equal
+ * -1: less than
+ * 1: bigger than
+ *
+ */
+static int COMPARISON_RS_EQUAL = 0;
+static int COMPARISON_RS_LESS = -1;
+static int COMPARISON_RS_BIGGER = 1;
+
 extern LogicalType DDGeoType;
 extern const LogicalType DDJsonType;
 extern const LogicalType DDNumericType;
